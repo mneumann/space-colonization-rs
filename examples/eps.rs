a65775b97a5b251d57a925f5409ea0a41685fcc7
Michael Neumann <mneumann@ntecs.de>
2016-01-10 17:04:43 +0100

    Move common code into examples/common/mod.rs

--- examples/eps.rs
+++ examples/eps.rs
@@ -5,116 +5,14 @@ extern crate num;
 extern crate clap;
 extern crate eps_writer;
 
-use na::{Pnt2, Vec2, Pnt3, FloatPnt, FloatVec};
-use rand::{Rng, Closed01};
+use na::{Pnt2, Vec2, FloatPnt, FloatVec};
 use num::Zero;
-use clap::{Arg, App};
-use std::str::FromStr;
 use space_colonization::{SpaceColonization, SqDist};
 use eps_writer::{EpsDocument, Position, Points, Lines, SetRGB};
 use std::fs::File;
+use common::{MyPoint, Config};
 
-fn random_closed01<R: Rng>(rng: &mut R) -> f32 {
-    rng.gen::<Closed01<f32>>().0
-}
-
-fn random_coord<R: Rng>(rng: &mut R) -> f32 {
-    2.0 * random_closed01(rng) - 1.0
-}
-
-trait MyPoint {
-    fn into_pnt3(self) -> Pnt3<f32>;
-    fn random<R: Rng>(rng: &mut R) -> Self;
-}
-
-impl MyPoint for Pnt3<f32> {
-    fn into_pnt3(self) -> Pnt3<f32> {
-        self
-    }
-
-    fn random<R: Rng>(rng: &mut R) -> Pnt3<f32> {
-        Pnt3::new(random_coord(rng), random_coord(rng), random_coord(rng))
-    }
-}
-
-impl MyPoint for Pnt2<f32> {
-    fn into_pnt3(self) -> Pnt3<f32> {
-        Pnt3::new(self.x, self.y, 3.0)
-    }
-
-    fn random<R: Rng>(rng: &mut R) -> Pnt2<f32> {
-        Pnt2::new(random_coord(rng), random_coord(rng))
-    }
-}
-
-#[derive(Debug)]
-struct Config {
-    n_attraction_points: usize,
-    n_roots: usize,
-    influence_radius: f32,
-    move_distance: f32,
-    kill_distance: f32,
-    use_3d: bool,
-    max_iter: Option<usize>,
-    save_every: Option<usize>,
-}
-
-impl Config {
-    fn from_cmd() -> Config {
-        let matches = App::new("space-colonization")
-                          .arg(Arg::with_name("NUM_POINTS")
-                                   .long("num-points")
-                                   .help("Number of attraction points (default: 1000)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("NUM_ROOTS")
-                                   .long("num-roots")
-                                   .help("Number of root nodes (default: 1)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("IR")
-                                   .long("radius")
-                                   .help("Influence radius (default: 0.25)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("KD")
-                                   .long("kill-distance")
-                                   .help("Kill distance (default: 0.1)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("MD")
-                                   .long("move-distance")
-                                   .help("Move distance (default: 0.05)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("MAX_ITER")
-                                   .long("max-iter")
-                                   .help("Maximum iterations (default: infinite)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("SAVE_EVERY")
-                                   .long("save-every")
-                                   .help("Save a picture every n iterations (default: none)")
-                                   .takes_value(true)
-                                   .required(false))
-                          .arg(Arg::with_name("USE_3D")
-                                   .long("use-3d")
-                                   .help("Use 3d mode"))
-                          .get_matches();
-
-        Config {
-            n_attraction_points: usize::from_str(matches.value_of("NUM_POINTS").unwrap_or("1000"))
-                                     .unwrap(),
-            n_roots: usize::from_str(matches.value_of("NUM_ROOTS").unwrap_or("1")).unwrap(),
-            influence_radius: f32::from_str(matches.value_of("IR").unwrap_or("0.25")).unwrap(),
-            kill_distance: f32::from_str(matches.value_of("KD").unwrap_or("0.1")).unwrap(),
-            move_distance: f32::from_str(matches.value_of("MD").unwrap_or("0.05")).unwrap(),
-            use_3d: matches.is_present("USE_3D"),
-            max_iter: usize::from_str(matches.value_of("MAX_ITER").unwrap_or("INVALID")).ok(),
-            save_every: usize::from_str(matches.value_of("SAVE_EVERY").unwrap_or("INVALID")).ok(),
-        }
-    }
-}
+pub mod common;
 
 const SCALE: f32 = 400.0;
 

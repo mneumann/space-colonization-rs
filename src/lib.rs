1ba63287f232284cb8a61b40780722a7cf3c79da
Michael Neumann <mneumann@ntecs.de>
2016-01-10 13:59:01 +0100

    Move &mut reference to nodes inside loop and unindent

--- src/lib.rs
+++ src/lib.rs
@@ -206,64 +206,62 @@ impl<P, F, I> SpaceColonization<P, F, I>
         let use_last_nodes: usize = cmp::min(num_nodes, use_last_n_nodes.unwrap_or(num_nodes));
         let start_index = num_nodes - use_last_nodes;
 
-        {
+        // for each attraction_point, find the nearest node that it influences
+        let mut ap_idx = 0;
+        'outer: while ap_idx < self.attractors.len() {
+            let ap = self.attractors[ap_idx];
+
             let nodes = &mut self.nodes[start_index..];
 
-            // for each attraction_point, find the nearest node that it influences
-            let mut ap_idx = 0;
-            'outer: while ap_idx < self.attractors.len() {
-                let ap = self.attractors[ap_idx];
-
-                // find the node nearest to the `ap` attraction point
-                let mut nearest_node: Option<&mut Node<_, _, _>> = None;
-                let mut nearest_distance = ap.attract_dist;
-                let mut connect_node: Option<&mut Node<_, _, _>> = None;
-                for node in nodes.iter_mut() {
-                    if !node.is_active() {
-                        // The node has become inactive
-                        continue;
-                    }
-
-                    let dist = SqDist(node.position.sqdist(&ap.position));
-
-                    if dist < ap.connect_dist {
-                        // This node is within the connect radius of a node.
-                        // XXX: There might be a closer node, but we use
-                        // the first we find.
-                        connect_node = Some(node);
-                        // outside the node loop, we perform some action
-                        break;
-                    } else if dist < nearest_distance {
-                        // ```node``` is within the influence of the attraction point,
-                        // and it's closer than the currently closest node.
-                        nearest_distance = dist;
-                        nearest_node = Some(node);
-                    }
+            // find the node nearest to the `ap` attraction point
+            let mut nearest_node: Option<&mut Node<_, _, _>> = None;
+            let mut nearest_distance = ap.attract_dist;
+            let mut connect_node: Option<&mut Node<_, _, _>> = None;
+            for node in nodes.iter_mut() {
+                if !node.is_active() {
+                    // The node has become inactive
+                    continue;
                 }
 
-                if let Some(node) = connect_node {
-                    node.transmit_information(ap.information);
-                    match ap.connect_action {
-                        ConnectAction::KillAttractor => {
-                            // remove attraction point
-                            self.attractors.swap_remove(ap_idx);
-                            // and continue with "next" (without increasing ap_idx)
-                            continue 'outer;
-                        }
-                        ConnectAction::DisableFor {iterations: _} => {
-                            unimplemented!();
-                        }
-                    }
-                } else if let Some(node) = nearest_node {
-                    // update the force with the normalized vector towards the attraction point
-                    let v = (ap.position - node.position).normalize() * ap.strength;
-                    node.growth = node.growth + v;
-                    node.growth_count += 1;
-                }
+                let dist = SqDist(node.position.sqdist(&ap.position));
 
-                // go to next attractor point
-                ap_idx += 1;
+                if dist < ap.connect_dist {
+                    // This node is within the connect radius of a node.
+                    // XXX: There might be a closer node, but we use
+                    // the first we find.
+                    connect_node = Some(node);
+                    // outside the node loop, we perform some action
+                    break;
+                } else if dist < nearest_distance {
+                    // ```node``` is within the influence of the attraction point,
+                    // and it's closer than the currently closest node.
+                    nearest_distance = dist;
+                    nearest_node = Some(node);
+                }
             }
+
+            if let Some(node) = connect_node {
+                node.transmit_information(ap.information);
+                match ap.connect_action {
+                    ConnectAction::KillAttractor => {
+                        // remove attraction point
+                        self.attractors.swap_remove(ap_idx);
+                        // and continue with "next" (without increasing ap_idx)
+                        continue 'outer;
+                    }
+                    ConnectAction::DisableFor {iterations: _} => {
+                        unimplemented!();
+                    }
+                }
+            } else if let Some(node) = nearest_node {
+                // update the force with the normalized vector towards the attraction point
+                let v = (ap.position - node.position).normalize() * ap.strength;
+                node.growth = node.growth + v;
+                node.growth_count += 1;
+            }
+
+            // go to next attractor point
+            ap_idx += 1;
         }
 
         // now create new nodes
